6e0aef547fc54c15d14054ad69a5f4a9f4c6c818
alaa <alaa.hagrassy98@gmail.com>
2022-06-03 23:19:21 -0700

    complete-UserModel

--- src/Routers/AuthRouter.js
+++ src/Routers/AuthRouter.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router() 
-const {register} = require('../Controllers/AuthController')
+const {register , logIn , edit ,getUsers,getById} = require('../Controllers/AuthController')
 const {body , validationResult} = require('express-validator')
-const {validateSignUp} = require('../Validations/UserValidation')
+const {validateSignUp , valdateUpdateUser} = require('../Validations/UserValidation')
 
 router.post('/register' , validateSignUp, register)
+router.get('/login' , logIn)
+router.put('/:id' ,valdateUpdateUser, edit)
+router.get('/' , getUsers)
+router.get('/:id' ,getById)
 
 module.exports = router

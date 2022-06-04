661e6cdf19ffb02ca274fb672a12b6ee9c938bd6
alaa <alaa.hagrassy98@gmail.com>
2022-06-03 19:31:32 -0700

    register

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const passwordMongodbAtlas = process.env.passwordMongodbAtlas
 async function main() {
-  await mongoose.connect(`mongodb+srv://Netflix:${passwordMongodbAtlas}@cluster0.gbn4c.mongodb.net/?retryWrites=true&w=majority`);
+  await mongoose.connect(`mongodb+srv://Netflix:${passwordMongodbAtlas}@cluster0.gbn4c.mongodb.net/Netflixdb?retryWrites=true&w=majority`);
   console.log("connected to database successfully");
 }
 main().catch(err => console.log(err));
---
+++ src/Controllers/AuthController.js
@@ -0,0 +1,37 @@
+const UserModel = require('../Models/AuthModel')
+const {validationResult} = require('express-validator')
+
+//register function
+
+register = async (req , res , next)=>{
+    const {FirstName , LastName , email , password , cardNumber,expirationDate,securityCode} = req.body
+    const user = await UserModel.findOne({email})
+
+    // check if email Exist 
+    if(user){
+        return res.json({message:'User exist'})
+    }
+    //check if the cardNumber Exist
+    const card = UserModel.findOne({cardNumber})
+    if(card){
+        return res.json("Invalid Card Number")
+    }
+
+    const NewUser = new UserModel({
+        FirstName,LastName,email,password,cardNumber,expirationDate,securityCode
+    }).save()
+    .then(data=>{
+        const token = data.generatToken();
+        if(!token)
+        return res.json("error")
+        return res.json({token})
+    }).catch(err=>{
+        res.json({
+            message:err
+        })
+    })
+
+
+}
+
+module.exports = {register}

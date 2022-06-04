3794131d637c1db5f8f05724c1f4d73687c9d119
Mohamed Essam <mohamedessam9798@gmail.com>
2022-06-04 16:39:51 +0200

    adding list model

---
+++ src/Models/ListModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const ListSchema = new mongoose.Schema(
+    {
+        title:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        type:{
+            type:String,
+        },
+        genre:{
+            type:String,
+        },
+        content:{
+            type:Array
+        },
+    }
+)
+module.exports= mongoose.model("List" , ListSchema);

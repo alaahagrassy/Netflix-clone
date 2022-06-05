933b3c6713196be3948eb1d67b823fd13906da76
alaa <alaa.hagrassy98@gmail.com>
2022-06-05 00:32:53 -0700

    add-middlewares

--- src/Routers/AuthRouter.js
+++ src/Routers/AuthRouter.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router() 
 const {register , logIn , edit ,getUsers,getById} = require('../Controllers/AuthController')
-const {body , validationResult} = require('express-validator')
-const {validateSignUp , valdateUpdateUser} = require('../Validations/UserValidation')
+const {validateSignUp , valdateUpdateUser } = require('../Validations/UserValidation')
+const {authorizeUser} = require('../middlewares/AuthorizeUser')
+const {checkForLoggedIn} = require('../middlewares/CheckForLogged')
 
-router.post('/register' , validateSignUp, register)
-router.get('/login' , logIn)
-router.put('/:id' ,valdateUpdateUser, edit)
+router.post('/register' , checkForLoggedIn,validateSignUp, register)
+router.get('/login' ,checkForLoggedIn, logIn)
+router.put('/:id' ,authorizeUser,valdateUpdateUser, edit)
 router.get('/' , getUsers)
 router.get('/:id' ,getById)
 

6620e84afdee34e1fdd4a32f1752162113d7a325
alaa <alaa.hagrassy98@gmail.com>
2022-06-19 23:02:01 -0700

    Edit-user

--- src/Validations/UserValidation.js
+++ src/Validations/UserValidation.js
@@ -1,43 +1,54 @@
 const Joi = require('@hapi/joi')
 // Joi.objectId = require('joi-objectid')(Joi)
 
-const signup =Joi.object({
+const signup = Joi.object({
     userName: Joi.string(),
-    email : Joi.string()
-    .email({
-        minDomainSegments: 2,
-        tlds: { allow: ["com", "net"] },
-    }).required(),
+    email: Joi.string()
+        .email({
+            minDomainSegments: 2,
+            tlds: { allow: ["com", "net"] },
+        }).required(),
     password: Joi.string().required().min(6),
-    cardNumber:Joi.number(),
-    expirationDate:Joi.date(),
-    securityCode:Joi.number().min(3).max(4),
-    isAdmin: Joi.boolean()
+    isAdmin: Joi.boolean(),
 })
 
 const UpdateUser = Joi.object({
     userName: Joi.string(),
-    email : Joi.string()
-    .email({
-        minDomainSegments: 2,
-        tlds: { allow: ["com", "net"] },
-    }),
+    email: Joi.string()
+        .email({
+            minDomainSegments: 2,
+            tlds: { allow: ["com", "net"] },
+        }),
     password: Joi.string().min(6),
 })
 
-const validateSignUp = (req , res  , next)=>{
-    const validation = signup.validate(req.body)
-    if(validation.error){
-      return res.json(validation.error.message)
-    }
-    next();
-};
+const Userpayment = Joi.object({
+    userName: Joi.string(),
+    cardNumber: Joi.number().required(),
+    expirationDate: Joi.date().required(),
+    securityCode: Joi.number().min(3).max(4).required(),
+})
 
-const valdateUpdateUser = (req , res , next)=>{
-    const validation = UpdateUser.validate(req.body)
-    if(validation.error){
+const paymentValidation = (req, res, next) => {
+    const validation = Userpayment.validate(req.body)
+    if (validation.error) {
         return res.json(validation.error.message)
     }
     next();
 }
-module.exports = {validateSignUp,valdateUpdateUser}
+const validateSignUp = (req, res, next) => {
+    const validation = signup.validate(req.body)
+    if (validation.error) {
+        return res.json(validation.error.message)
+    }
+    next();
+};
+
+const valdateUpdateUser = (req, res, next) => {
+    const validation = UpdateUser.validate(req.body)
+    if (validation.error) {
+        return res.json(validation.error.message)
+    }
+    next();
+}
+module.exports = { validateSignUp, valdateUpdateUser, paymentValidation }
--- src/middlewares/AuthorizeUser.js
+++ src/middlewares/AuthorizeUser.js
@@ -4,8 +4,6 @@ const authorizeUser = async (req , res , next)=>{
     const {id} = req.params
    const token = await req.headers.authorization
     const user = await UserModel.getCurrentUser(token)
-    console.log(user);
-    console.log(user.id);
     if(id !== user.id)
     return res.json('You are not authorized to complete this action')
     next()

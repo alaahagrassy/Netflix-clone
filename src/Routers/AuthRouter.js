3fe00d9365881641fad87e3595695c21352e978d
alaa <alaa.hagrassy98@gmail.com>
2022-06-28 00:43:25 -0700

    edit

--- src/Routers/AuthRouter.js
+++ src/Routers/AuthRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router() 
-const {register , logIn , edit ,getUsers,getById,Remove ,payment,plan,destroy,devices,watchedMovies,removeDevice,DeletFav,logOut,FavMovies} = require('../Controllers/AuthController')
+const {register , logIn , edit ,getUsers,getById,Remove ,payment,plan,getuser,editForAdmin,destroy,devices,watchedMovies,removeDevice,DeletFav,logOut,FavMovies} = require('../Controllers/AuthController')
 const {validateSignUp , valdateUpdateUser,paymentValidation } = require('../Validations/UserValidation')
 const {authorizeUser} = require('../middlewares/AuthorizeUser')
 const {checkForLoggedIn} = require('../middlewares/CheckForLogged')
@@ -9,8 +9,8 @@ const {checkForLoggedIn} = require('../middlewares/CheckForLogged')
 router.post('/register' , checkForLoggedIn,validateSignUp, register)
 router.post('/login' ,checkForLoggedIn ,logIn)
 router.put('/edit',authorizeUser, edit)
-router.get('/all', getUsers)
 router.get('/getuser' ,authorizeUser,getById)
+
 router.delete('/:id' ,Remove)
 router.put('/payment' , authorizeUser,paymentValidation,payment)
 router.put('/Plan' ,authorizeUser,plan)
@@ -23,4 +23,10 @@ router.put('/fav',authorizeUser,FavMovies)
 router.put('/watched',authorizeUser,watchedMovies)
 router.put('/delFav',authorizeUser,DeletFav)
 
+
+//forAdmin
+
+router.put('/:id', editForAdmin)
+router.get('/all', getUsers)
+router.get('/:id' ,getuser)
 module.exports = router

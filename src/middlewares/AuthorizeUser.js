0932ced39741e51cef56f222b86ee13ed9c6224b
alaa <alaa.hagrassy98@gmail.com>
2022-07-01 04:12:03 -0700

    edit

--- src/middlewares/AuthorizeUser.js
+++ src/middlewares/AuthorizeUser.js
@@ -1,7 +1,10 @@
 const UserModel = require('../Models/AuthModel')
 
 const authorizeUser = async (req , res , next)=>{
+
    const token =  req.headers.authorization
+   if(!token)
+   return res.json('You are not authorized')
     const user = await UserModel.getCurrentUser(token)
     if(!user.id)
     return res.json('You are not authorized to complete this action')
